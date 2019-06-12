b2e96fc26e040192b1abf07cdd0cab30e0456b5e
Francois Brassart <francois.brassart@polytech-lille.net>
2019-06-12 04:52:26 -0700

    modifications mineures

--- src/octoprint/printer/profile.py
+++ src/octoprint/printer/profile.py
@@ -209,8 +209,8 @@ class PrinterProfileManager(object):
 
 	default = dict(
 		id = "_default",
-		name = "Default",
-		model = "Generic RepRap Printer",
+		name = "DefaultTizyx",
+		model = "Generic Tizyx Printer",
 		color = "default",
 		volume=dict(
 			width = 200,
