a9152879ed85f3da0ee052773d60d6321de1b129
TiZYX4 <francoisb541@hotmail.fr>
2019-06-24 06:22:41 -0700

    suppression du tab terminal

--- src/octoprint/server/views.py
+++ src/octoprint/server/views.py
@@ -572,8 +572,7 @@ def fetch_template_data(refresh=False):
 
 	templates["tab"]["entries"] = dict(
 		control=(gettext("Control"), dict(template="tabs/control.jinja2", _div="control")),
-		gcodeviewer=(gettext("GCode Viewer"), dict(template="tabs/gcodeviewer.jinja2", _div="gcode")),
-		terminal=(gettext("Terminal"), dict(template="tabs/terminal.jinja2", _div="term"))
+		gcodeviewer=(gettext("GCode Viewer"), dict(template="tabs/gcodeviewer.jinja2", _div="gcode"))
 	)
 
 	# settings dialog
